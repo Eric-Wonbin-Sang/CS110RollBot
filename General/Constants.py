590b692037ae65ce82bef74435f0ec44cd36e667
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2021-04-08 15:32:09 -0400

    Added example script for Discord Bot. Token file added to Secrets.

---
+++ General/Constants.py
@@ -0,0 +1,8 @@
+import os
+
+
+project_dir = "/".join(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/").split("/")[:-1])
+projects_dir = "/".join(project_dir.split("/")[:-1])
+secrets_dir = projects_dir + "/" + "0 - Secrets"
+
+token = open(secrets_dir + "/CS110RollBot/token.txt").read()
